f1db37bd4dff1a3bfb239997ae3f9eb11c82f74d
Sebastián Peralta Rodríguez <115424131+SebasCody@users.noreply.github.com>
2024-06-18 23:54:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
     
-    uvicorn.run("main:app", host="0.0.0.0", port=4000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=4000, reload=True, log_level="info")
